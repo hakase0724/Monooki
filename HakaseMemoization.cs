d6de64c668f0fdc30c1a7af88e00007e02bd22cd
hakase0724 <35761029+hakase0724@users.noreply.github.com>
2019-09-20 22:57:16 +0900

    ILde

---
+++ HakaseMemoization.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Diagnostics;
+using ILUtility;
+
+namespace Hakase
+{
+    public static class Memoization
+    {
+        private static Dictionary<Type, Type> sTypeDictionary = new Dictionary<Type, Type>();
+        private static AssemblyBuilder sAssemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(cAssemblyName), AssemblyBuilderAccess.RunAndSave);
+        private static ModuleBuilder sModuleBuilder = sAssemblyBuilder.DefineDynamicModule(cAssemblyName, cAssemblyName + ".dll");
+
+        private const string cAssemblyName = "HakaseMemoization";
+
+        public static T Create<T>(params object[] parameters)
+        {
+            if (!sTypeDictionary.TryGetValue(typeof(T), out Type type))
+            {
+                //指定したクラスを親とするクラスを作る
+                var typeBuilder = sModuleBuilder.DefineType(cAssemblyName + Guid.NewGuid(), TypeAttributes.Class, typeof(T));
+                //メモに使うDictionaryの型
+                var dictionaryType = typeof(Dictionary<Param, T>);
+                //メモ用Dictionaryを定義する
+                var dictionaryField = typeBuilder.DefineField(
+                    "Field_" + Guid.NewGuid().ToString("N"),
+                    dictionaryType,
+                    FieldAttributes.Private);
+
+                //dictionaryField.SetValue(dictionaryField, new Dictionary<Param, T>());
+                //それぞれメソッド取得
+                var dictionaryConstructor = dictionaryType.GetConstructor(Type.EmptyTypes);
+                var tryGetValue = dictionaryType.GetMethod("TryGetValue");
+                var setItem = dictionaryType.GetMethod("set_Item");
+                var paramConstructor = typeof(Param).GetConstructors()[0];
+                //メモ化できるメソッドを取得
+                var methods = typeof(T)
+                    .GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                    .Where(a => a.IsVirtual && !a.IsFinal)
+                    .Where(a => a.ReturnParameter.ParameterType != typeof(void))
+                    .Where(a => a.GetParameters().Length > 0)
+                    .Where(a => a.GetCustomAttribute<HakaseMemoizationAttribute>() != null);
+
+                //コンストラクタ作る
+                var ctor = typeBuilder.DefineConstructor
+                    (MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName,
+                    CallingConventions.Standard,
+                    null);
+                //IL打ち込む
+                var ctorIl = ctor.GetILGenerator();
+                //ローカル変数宣言
+                ctorIl.DeclareLocal(dictionaryType);
+                //継承元であるobjcetのコンストラクターを呼ぶ
+                ctorIl.Emit(OpCodes.Ldarg_0);
+                ctorIl.Emit(OpCodes.Call, typeof(T).GetConstructor(Type.EmptyTypes));
+                //Dictionaryの生成を行う
+                ctorIl.Emit(OpCodes.Ldarg_0);
+                ctorIl.Emit(OpCodes.Newobj, dictionaryConstructor);
+                ctorIl.Emit(OpCodes.Stfld, dictionaryField);
+                ctorIl.Emit(OpCodes.Ret);
+
+
+                //実際にメモ化するILを吐く
+                foreach (var method in methods)
+                {
+                    //引数の型を取得
+                    var parameterTypes = method
+                        .GetParameters()
+                        .Select(a => a.ParameterType)
+                        .ToArray();
+                    //継承元となるメソッドのアクセス修飾子によってつけるものを変える
+                    var methodAttributes = method.IsAbstract
+                        ? MethodAttributes.Public | MethodAttributes.Virtual
+                        : method.Attributes;
+                    //メソッド定義
+                    var methodBuilder = typeBuilder.DefineMethod(
+                        method.Name,
+                        methodAttributes,
+                        method.CallingConvention,
+                        method.ReturnParameter.ParameterType,
+                        parameterTypes);
+
+                    //こっからはいつもどおりILを打ち込んでいく
+                    var il = methodBuilder.GetILGenerator();
+
+                    // ローカル変数の宣言
+                    il.DeclareLocal(typeof(Param));
+                    il.DeclareLocal(method.ReturnParameter.ParameterType); // result
+                    il.DeclareLocal(typeof(bool));
+                    il.DeclareLocal(typeof(bool));
+
+                    // ラベルの宣言
+                    var label2 = il.DefineLabel();
+                    var label3 = il.DefineLabel();
+
+                    // object[] を用意
+                    il.LdcI4(parameterTypes.Length);
+                    il.Emit(OpCodes.Newarr, typeof(object));
+
+                    // object[] に引数をセット
+                    for (int i = 0; i < parameterTypes.Length; i++)
+                    {
+                        il.Emit(OpCodes.Dup);
+                        il.LdcI4(i);
+                        il.Ldarg(i + 1);
+                        if (parameterTypes[i].IsValueType)
+                        {
+                            il.Emit(OpCodes.Box, parameterTypes[i]);
+                        }
+                        il.Emit(OpCodes.Stelem_Ref);
+                    }
+
+                    // Param を作成してローカル変数 0 に保存
+                    il.Emit(OpCodes.Newobj, paramConstructor);
+                    il.Stloc(0);
+
+                    // dictionary.TryGetValue(param, out result)
+                    il.Emit(OpCodes.Ldarg_0);
+                    il.Emit(OpCodes.Ldfld, dictionaryField);
+                    il.Emit(OpCodes.Ldloc_0);
+                    il.Emit(OpCodes.Ldloca_S,1);
+                    il.Emit(OpCodes.Callvirt, tryGetValue);
+                    il.Emit(OpCodes.Stloc_3);
+
+                    // TryGetValue の結果が false なら label2 へ
+                    il.Emit(OpCodes.Ldloc_3);
+                    il.Emit(OpCodes.Brfalse_S, label2);
+
+                    // リターン位置へジャンプ
+                    il.Emit(OpCodes.Br_S, label3);
+
+                    il.MarkLabel(label2);
+
+                    // 継承したメソッドの実行
+                    for (int i = 0; i < parameterTypes.Length + 1; i++)
+                    {
+                        il.Ldarg(i);
+                    }
+                    il.Emit(OpCodes.Call, method);
+                    il.Emit(OpCodes.Stloc_1);
+
+                    // メモ
+                    il.Emit(OpCodes.Ldarg_0);
+                    il.Emit(OpCodes.Ldfld, dictionaryField);
+                    il.Emit(OpCodes.Ldloc_0);
+                    il.Emit(OpCodes.Ldloc_1);
+                    il.Emit(OpCodes.Callvirt, setItem);
+
+                    il.MarkLabel(label3);
+
+                    il.Emit(OpCodes.Ldloc_1);
+                    il.Emit(OpCodes.Ret);
+                    typeBuilder.DefineMethodOverride(methodBuilder, method);
+                }
+                type = typeBuilder.CreateType();
+                sTypeDictionary[typeof(T)] = type;
+                //デバッグのためセーブ
+                sAssemblyBuilder.Save(cAssemblyName + ".dll");
+                ILUtility.ILUtility.Verify(sAssemblyBuilder);
+            }           
+            return (T)Activator.CreateInstance(type, parameters);
+        }
+
+        public class Param 
+        {
+            private object[] items;
+            private int hashCode = 0;
+
+            public Param(params object[] items)
+            {
+                this.items = items;
+                for(int i = 0;i < Count;i++)
+                {
+                    hashCode ^= items[i] == null ? 0 : items[i].GetHashCode();
+                }
+            }
+
+            public int Count { get => items.Length; }
+
+            public override bool Equals(object obj)
+            {
+                var param = obj as Param;
+                if (param == null) return false;
+                if (Count != param.Count) return false;
+                if (hashCode != param.GetHashCode()) return false;
+                return true;
+            }
+
+            public override int GetHashCode()
+            {
+                return hashCode;
+            }
+        }
+    }
+
+    public class HakaseMemoizationAttribute : Attribute { }
+
+    public class HakaseMemo
+    {
+        public void MeasureTime(string title, int index)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int result = Fibonacci(index);
+            stopwatch.Stop();
+            Console.WriteLine($"[{title}]");
+            Console.WriteLine($"Index: {index}");
+            Console.WriteLine($"Answer: {result}");
+            Console.WriteLine($"Time: {stopwatch.Elapsed}");
+            Console.WriteLine("");
+        }
+
+        [HakaseMemoization]
+        public virtual int Fibonacci(int index)
+        {
+            if (index < 0) throw new ArgumentException();
+            return index < 2 ? index : Fibonacci(index - 1) + Fibonacci(index - 2);
+        }
+    }
+}
---
+++ ILUtility.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ILUtility
+{
+    static class ILUtility
+    {
+        public static void Verify(params AssemblyBuilder[] builders)
+        {
+            var path = @"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\PEVerify.exe";
+
+            foreach (var targetDll in builders)
+            {
+                var psi = new ProcessStartInfo(path, targetDll.GetName().Name + ".dll")
+                {
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false
+                };
+
+                var p = Process.Start(psi);
+                var data = p.StandardOutput.ReadToEnd();
+                Console.WriteLine(data);
+            }
+        }
+
+        /// <summary>
+        /// 第一引数を取得
+        /// </summary>
+        /// <param name="il"></param>
+        /// <param name="method"></param>
+        public static void LdargFirst(this ILGenerator il, MethodBase method)
+        {
+            //staticメソッドならldarg.0
+            if (method.IsStatic) il.Emit(OpCodes.Ldarg_0);
+            //インスタンスメソッドならldarg.1
+            else il.Emit(OpCodes.Ldarg_1);
+        }
+
+        /// <summary>
+        /// インスタンスメソッドのthisを取得する
+        /// </summary>
+        /// <param name="il"></param>
+        /// <param name="method"></param>
+        public static void LdargThis(this ILGenerator il, MethodBase method)
+        {
+            if (method.IsStatic) throw new System.ArgumentException();
+            il.Emit(OpCodes.Ldarg_0);
+        }
+
+        public static void Ldarg(this ILGenerator il,int value)
+        {
+            switch (value)
+            {
+                case 0:
+                    il.Emit(OpCodes.Ldarg_0);
+                    break;
+                case 1:
+                    il.Emit(OpCodes.Ldarg_1);
+                    break;
+                case 2:
+                    il.Emit(OpCodes.Ldarg_2);
+                    break;
+                case 3:
+                    il.Emit(OpCodes.Ldarg_3);
+                    break;
+                default:
+                    if (value <= 255 && value >= 4) il.Emit(OpCodes.Ldarg_S, (byte)value);
+                    else il.Emit(OpCodes.Ldarg, value);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 指定した引数を取得
+        /// </summary>
+        /// <param name="il"></param>
+        /// <param name="method"></param>
+        /// <param name="value"></param>
+        public static void Ldarg(this ILGenerator il, MethodBase method,int value)
+        {
+            //インスタンスメソッドなら第一引数はthisのため一個ずらす
+            if (!method.IsStatic) value++;
+
+            switch (value)
+            {
+                case 0:
+                    il.Emit(OpCodes.Ldarg_0);
+                    break;
+                case 1:
+                    il.Emit(OpCodes.Ldarg_1);
+                    break;
+                case 2:
+                    il.Emit(OpCodes.Ldarg_2);
+                    break;
+                case 3:
+                    il.Emit(OpCodes.Ldarg_3);
+                    break;
+                default:
+                    if (value <= 255 && value >= 4) il.Emit(OpCodes.Ldarg_S, (byte)value);
+                    else il.Emit(OpCodes.Ldarg, value);
+                    break;
+            }
+        }
+
+        public static void LdcI4(this ILGenerator il,int value)
+        {
+            //-1～8までは専用のコードが存在する
+            //sbyteまでの範囲ならsbyteにした方が最適化がかかるらしい
+            switch (value)
+            {
+                case -1:
+                    il.Emit(OpCodes.Ldc_I4_M1);
+                    break;
+                case 0:
+                    il.Emit(OpCodes.Ldc_I4_0);
+                    break;
+                case 1:
+                    il.Emit(OpCodes.Ldc_I4_1);
+                    break;
+                case 2:
+                    il.Emit(OpCodes.Ldc_I4_2);
+                    break;
+                case 3:
+                    il.Emit(OpCodes.Ldc_I4_3);
+                    break;
+                case 4:
+                    il.Emit(OpCodes.Ldc_I4_4);
+                    break;
+                case 5:
+                    il.Emit(OpCodes.Ldc_I4_5);
+                    break;
+                case 6:
+                    il.Emit(OpCodes.Ldc_I4_6);
+                    break;
+                case 7:
+                    il.Emit(OpCodes.Ldc_I4_7);
+                    break;
+                case 8:
+                    il.Emit(OpCodes.Ldc_I4_8);
+                    break;
+                default:
+                    if (value <= 127 && value >= -127) il.Emit(OpCodes.Ldc_I4_S, (sbyte)value);
+                    else il.Emit(OpCodes.Ldc_I4, value);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 仮想メソッドか否かで呼ぶcallを切り替える
+        /// </summary>
+        /// <param name="il"></param>
+        /// <param name="method"></param>
+        public static void Call(this ILGenerator il,MethodInfo method)
+        {
+            if (method.IsFinal || !method.IsVirtual) il.Emit(OpCodes.Call, method);
+            else il.Emit(OpCodes.Callvirt, method);
+        }
+
+        /// <summary>
+        /// 保存されているローカルの値を取り出す
+        /// </summary>
+        /// <param name="il"></param>
+        /// <param name="value"></param>
+        public static void Ldloc(this ILGenerator il, int value)
+        {
+            switch (value)
+            {
+                case 0:
+                    il.Emit(OpCodes.Ldloc_0);
+                    break;
+                case 1:
+                    il.Emit(OpCodes.Ldloc_1);
+                    break;
+                case 2:
+                    il.Emit(OpCodes.Ldloc_2);
+                    break;
+                case 3:
+                    il.Emit(OpCodes.Ldloc_3);
+                    break;
+                default:
+                    if (value <= 255 && value >= 0) il.Emit(OpCodes.Ldloc_S, (byte)value);
+                    else il.Emit(OpCodes.Ldloc, value);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// ローカルに値を保存する
+        /// </summary>
+        /// <param name="il"></param>
+        /// <param name="value"></param>
+        public static void Stloc(this ILGenerator il, int value)
+        {
+            switch (value)
+            {
+                case 0:
+                    il.Emit(OpCodes.Stloc_0);
+                    break;
+                case 1:
+                    il.Emit(OpCodes.Stloc_1);
+                    break;
+                case 2:
+                    il.Emit(OpCodes.Stloc_2);
+                    break;
+                case 3:
+                    il.Emit(OpCodes.Stloc_3);
+                    break;
+                default:
+                    if (value <= 255 && value >= 0) il.Emit(OpCodes.Stloc_S, (byte)value);
+                    else il.Emit(OpCodes.Stloc, value);
+                    break;
+            }
+        }
+    }
+}
